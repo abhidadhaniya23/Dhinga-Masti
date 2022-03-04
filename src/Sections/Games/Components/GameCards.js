060434626d72875085173c0c871e93374e0946bd
Rupen Bhimani <rupenbhimani109@gmail.com>
2022-03-04 17:46:42 +0530

    updated games section

--- src/Sections/Games/Components/GameCards.js
+++ src/Sections/Games/Components/GameCards.js
@@ -12,7 +12,7 @@ const GamesCards = () => {
                     <h2 class="text-gray-900 text-lg title-font font-medium">Shooting Stars</h2>
                 </div>
                 {/* <img src={childPicture} className="w-[30rem] pt-12 relative left-[3rem] mx-auto pb-[18rem]" alt="" /> */}
-
+             
                 <p class=" leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast
                     vegan taxidermy. Gastropub indxgo juice poutine.Blue bottle crucifix vinyl post-ironic four
                     dollar toast
--- src/Sections/Games/Components/MultipleGames.js
+++ src/Sections/Games/Components/MultipleGames.js
@@ -11,7 +11,6 @@ const MultipleGames = () =>{
       <GamesCards />
       <GamesCards />
       </div>
-      
       </>
     );
 };
