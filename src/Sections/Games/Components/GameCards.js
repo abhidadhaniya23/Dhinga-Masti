8c7ec6d1f0ea5d73a4a8bf90b46b28bf09b72e8e
Rupen Bhimani <rupenbhimani109@gmail.com>
2022-03-07 22:14:57 +0530

    update game section

--- src/Sections/Games/Components/GameCards.js
+++ src/Sections/Games/Components/GameCards.js
@@ -5,21 +5,32 @@ import games from "../../../Assets/games.jpeg";
 const GamesCards = () => {
 
     return (
-        <div className="bg-white sm:w-[56rem] sm:h-fit w-80  h-[8rem]  flex flex-row m-4 mx-auto sm:mt-14 mt-12 odd:flex-row-reverse rounded-xl overflow-hidden">
-            <div className="sm:flex">
-                <img className="object-cover w-[48rem] h-32 sm:w-[42rem] sm:h-[15rem]" src={games}/>
+        // <div className="bg-white  flex flex-row m-4 mx-auto sm:mt-14 mt-12 odd:flex-row-reverse rounded-xl overflow-hidden">
+        //     <div className="md:flex absolute w-52 h-52 ">
+        //     </div>
+        //     <div className="flex flex-col sm:mt-3 sm:p-4 ml-9 overflow-hidden sm:text-base text-xs ">
+        //         <div className="flex flex-col ">
+        //         </div>
+        //     </div>
+        // </div>
+        
+        <div className="flex flex-row w-1/2 h-56 my-4 rounded-xl bg-gray-300 odd:flex-row-reverse mx-auto justify-center items-center  overflow-hidden">
+            <div className="w-full h-full">
+                <img className="object-cover h-full w-full" src={games} />
             </div>
-            <div className="flex flex-col sm:mt-3 sm:p-4 ml-9 overflow-hidden sm:text-base text-xs ">
-                <p className="sm:text-2xl text-sm  mt-4"> Shooting Stars</p>
-                <div className=" h-1 w-16 mt-2  mb-3 bg-yellow-400 rounded text-black"></div>
-                <div className="flex flex-col">
-                    Gastropub indxgo juice poutine.Blue bottle crucifix vinyl post-ironic four
-                    dollar toast vegan taxidermy. Gastropub indxgo juice poutine.Blue bottle crucifix vinyl post-ironic four
-                    dollar toast  gan taxidermy.
-                <button className="items-center justify-center w-36 mt-4 px-1 py-1  text-xl text-gray-900 rounded-full cursor-pointer outline focus:outline-offset-4 outline-offset-2 outline-gray-200 font-bolder bg-emerald-200">Play Games</button>    
-                </div>
+            <div className="flex flex-col justify-center items-start px-8">
+                <h2 className="sm:text-2xl text-sm"> Shooting Stars</h2>
+                <span className=" h-1 w-16 mt-2  mb-3 bg-yellow-400 rounded text-black"></span>
+                Gastropub indxgo juice poutine.Blue bottle crucifix vinyl post-ironic four
+                dollar toast vegan taxidermy. Gastropub indxgo juice poutine.Blue bottle crucifix vinyl post-ironic four
+                Gastropub indxgo juice poutine.
+                
+            
+            <button className="items-center justify-center w-36 mt-4 px-1 py-1 text-xl text-gray-900 rounded-full cursor-pointer outline focus:outline-offset-4 outline-offset-2 outline-gray-200 font-bolder bg-emerald-200">Play Games</button>    
             </div>
         </div>
     );
 };
 export default GamesCards;
+
+
