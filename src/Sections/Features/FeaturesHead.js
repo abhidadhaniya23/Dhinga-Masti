28dd094a3311cfe685ff5a36cadc7d0f25f97ee9
Rupen Bhimani <rupenbhimani109@gmail.com>
2022-03-07 17:50:04 +0530

    Add Content To Our team

--- src/Sections/Features/FeaturesHead.js
+++ src/Sections/Features/FeaturesHead.js
@@ -3,16 +3,40 @@ import Card from "./Card";
 // import Polygon from "../../Assets/Polygon.png";
 
 const FeaturesHead = () => {
+    const cards= [
+        {
+            title: "OurAim",
+            desc: "Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat Photo booth fam kinfolk cold-pressed srir",
+        },
+
+        {
+            title: "Objective",
+            desc: "jane meri jane man ssed sriracha leggings jianbing microdosing tousled waistcoat Photo booth fam kinfolk cold-pressed srir",
+        },
+
+        {
+            title: "Bharosa",
+            desc: "bewafa tera masoom chehra Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat Photo boo",
+        },
+
+        {
+            title: "trust",
+            desc: "tuje kitna chahne lage hum Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat Photo boo",
+        },
+
+        {
+            title: "dream",
+            desc: "taro ke saher me dharti pe ..Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat Photo",
+        },
+    ];
     return (
         <>
             <section className="relative w-full min-h-screen overflow-x-hidden bg-fixed bg-polygonScatter">
                 <h1 className="text-center pt-24">FEATURES</h1>
                 <div className="flex flex-wrap justify-center items-center container px-16 pt-10 backdrop-blur-md">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                    {cards.map((card, index) => (
+                     <Card title={card.title} key={index} index={index} desc={card.desc}></Card>   
+                    ))}
                 </div>
             </section>
         </>
