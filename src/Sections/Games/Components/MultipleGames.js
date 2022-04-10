f9e24506ff3e21b90837e04b9a5426a4ca2df3b5
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-04-11 00:06:06 +0530

    change title and add content of games

--- src/Sections/Games/Components/MultipleGames.js
+++ src/Sections/Games/Components/MultipleGames.js
@@ -9,61 +9,61 @@ import img6 from "../../../Assets/games/6.png";
 import img7 from "../../../Assets/games/7.png";
 
 const MultipleGames = () => {
-  const gamesData = [
-    {
-      title: "2048",
-      link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/2048/index.html",
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sapiente alias facere cum distinctio corporis sint iure tempora error ullam, explicabo expedita, recusandae dolorem deserunt ipsa dolores similique, nulla pariatur.",
-      img: img6,
-    },
-    {
-      title: "Color-Cubes",
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sapiente alias facere cum distinctio corporis sint iure tempora error ullam, explicabo expedita, recusandae dolorem deserunt ipsa dolores similique, nulla pariatur.",
-      link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/Color-Cubes/index.html",
-      img: img1,
-    },
-    {
-      title: "Color-Game",
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sapiente alias facere cum distinctio corporis sint iure tempora error ullam, explicabo expedita, recusandae dolorem deserunt ipsa dolores similique, nulla pariatur.",
-      link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/Color-Games/index.html",
-      img: img3,
-    },
-    {
-      title: "Digital-Puzzle",
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sapiente alias facere cum distinctio corporis sint iure tempora error ullam, explicabo expedita, recusandae dolorem deserunt ipsa dolores similique, nulla pariatur.",
-      link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/Digital-Puzzle/index.html",
-      img: img4,
-    },
-    {
-      title: "Hextris",
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sapiente alias facere cum distinctio corporis sint iure tempora error ullam, explicabo expedita, recusandae dolorem deserunt ipsa dolores similique, nulla pariatur.",
-      link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/Hextris/index.html",
-      img: img7,
-    },
-    {
-      title: "Math-Game",
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sapiente alias facere cum distinctio corporis sint iure tempora error ullam, explicabo expedita, recusandae dolorem deserunt ipsa dolores similique, nulla pariatur.",
-      link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/Math-Game/index.html",
-      img: img2,
-    },
-    {
-      title: "Tetris",
-      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sapiente alias facere cum distinctio corporis sint iure tempora error ullam, explicabo expedita, recusandae dolorem deserunt ipsa dolores similique, nulla pariatur.",
-      link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/Tetris/index.html",
-      img: img5,
-    },
-  ];
-  return (
-    <>
-      <div id="games" className="w-full pb-24">
-        <div className="container mx-auto">
-          <h1 className="pt-16 pb-20 text-center"> Play Games </h1>
-          {gamesData.map((game, index) => (
-            <GamesCards title={game.title} key={index} desc={game.desc} img={game.img} link={game.link} />
-          ))}
-        </div>
-      </div>
-    </>
-  );
+    const gamesData = [
+        {
+            title: "Color-Game",
+            desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sapiente alias facere cum distinctio corporis sint iure tempora error ullam, explicabo expedita, recusandae dolorem deserunt ipsa dolores similique, nulla pariatur.",
+            link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/Color-Games/index.html",
+            img: img3,
+        },
+        {
+            title: "2048",
+            link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/2048/index.html",
+            desc: "2048 is an easy and fun puzzle game. Even if you don't love numbers you will love this game. It is played on a 4x4 grid using the arrows or W, A, S, D keys alternatively. Every time you press a key - all tiles slide. Tiles with the same value that bump into one-another are merged. Although there might be an optimal strategy to play, there is always some level of chance. If you beat the game and would like to master it, try to finish with a smaller score. That would mean that you finished with less moves.",
+            img: img6,
+        },
+        {
+            title: "Color-Cubes",
+            desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sapiente alias facere cum distinctio corporis sint iure tempora error ullam, explicabo expedita, recusandae dolorem deserunt ipsa dolores similique, nulla pariatur.",
+            link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/Color-Cubes/index.html",
+            img: img1,
+        },
+        {
+            title: "Digital-Puzzle",
+            desc: "Digital puzzle is a game in which the numbers are to be arranged in an ascending order. All you need to do is, move the blocks using arrow keys and finish the task. ",
+            link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/Digital-Puzzle/index.html",
+            img: img4,
+        },
+        {
+            title: "Hextris",
+            desc: "Hextris is a simple reincarnation of Tetris in hexagon form. Meaning that colored lines will fall from six sides towards a central hexagon, and you need to rotate the hexagon to get the colors to match. When you eventually can’t keep up, and the hexagon fills with colored lines, the game is over.",
+            link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/Hextris/index.html",
+            img: img7,
+        },
+        {
+            title: "Math-Game",
+            desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas sapiente alias facere cum distinctio corporis sint iure tempora error ullam, explicabo expedita, recusandae dolorem deserunt ipsa dolores similique, nulla pariatur.",
+            link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/Math-Game/index.html",
+            img: img2,
+        },
+        {
+            title: "Tetris",
+            desc: "Eliminate four lines at once and you officially achieve a Tetris Line Clear. Before a Tetrimino reaches the bottom of the Matrix, you can move and rotate this shape to fit your desired position. Keep in mind that moving your Tetrimino shapes depends on which device you use to play Tetris",
+            link: "https://youth-avenue-2021.github.io/Toy_Curio_Games/Tetris/index.html",
+            img: img5,
+        },
+    ];
+    return (
+        <>
+            <div id="games" className="w-full pb-24">
+                <div className="container mx-auto">
+                    <h1 className="pt-16 pb-20 text-center"> Play Games </h1>
+                    {gamesData.map((game, index) => (
+                        <GamesCards title={game.title} key={index} desc={game.desc} img={game.img} link={game.link} />
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 };
 export default MultipleGames;
--- src/Sections/Quote/Quote.js
+++ src/Sections/Quote/Quote.js
@@ -2,20 +2,20 @@ import React from "react";
 import childPicture from "../../Assets/child.png";
 
 const Quote = () => {
-  return (
-    <>
-      <section className="relative md:py-32 w-full">
-        <h1 className="text-center text-gray-900 pt-20">Childhood</h1>
-        <div className="container h-full mx-auto text-center font-text ">
-          <p className="w-3/5 px-5 mx-auto text-2xl mt-10 text-gray-900 shadow-lg rounded-lg py-7 backdrop-blur-2xl bg-white/10">
-            Children must think be taught how to think not what to think. <br /> - Megaret Mead
-          </p>
-          <img src={childPicture} className="w-[30rem] pt-12 relative left-[3rem] mx-auto" alt="" />
-          <span className="h-1 bg-gray-900 block w-full"></span>
-        </div>
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section className="relative w-full md:py-32">
+                <h1 className="pt-20 text-center text-gray-900">Childhood</h1>
+                <div className="container h-full mx-auto text-center font-text ">
+                    <p className="w-3/5 px-5 mx-auto mt-10 text-2xl text-gray-900 rounded-lg shadow-lg py-7 backdrop-blur-2xl bg-white/10">
+                        Children must think be taught how to think not what to think. <br /> - Margaret Mead
+                    </p>
+                    <img src={childPicture} className="w-[30rem] pt-12 relative left-[3rem] mx-auto" alt="" />
+                    <span className="block w-full h-1 bg-gray-900"></span>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default Quote;
